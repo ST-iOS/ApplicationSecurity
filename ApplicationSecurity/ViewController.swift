18ba612904de6572305d01c98d6c08df91b09a13
subash <thapa.subash345@gmail.com>
2020-09-01 20:13:22 -0700

    Initial Commit

---
+++ ApplicationSecurity/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ApplicationSecurity
+//
+//  Created by subash on 9/1/20.
+//  Copyright © 2020 symbolicTrace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
